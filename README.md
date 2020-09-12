f2134e295f01d09674d43d7fa3e73989e5e8bb81
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2020-09-11 20:31:23 -0600

    Created getOdds, test case, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # COSC499Rep
 Individual Git Exercise
+
+Method getOdds prints and returns a list of odd numbers from the inputted list.
--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-# TODO: Create function that prints only even integers
+import unittest
 
-# TODO: Create function that prints only odd integers
+testList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# TODO: Crate a function that returns a list of only even integers
+
+# Prints and Returns a list of only odd integers
+def getOdds(nums):
+    odds = list()
+
+    for num in nums:
+        if(num % 2 == 1):
+            odds.append(num)
+    
+    print(odds)
+    return odds
+
+class TestEvenOddMethods(unittest.TestCase):
+    def test_odds(self):
+        self.assertEqual(getOdds(testList), [1, 3, 5, 7, 9])
+
+if __name__ == '__main__':
+    unittest.main()
